69ef06169b5ad1763d18edba5951fb34250c2949
Sadik Shaikh <abdullahsadik00@gmail.com>
2024-08-19 12:12:16 +0530

    React Basic : Context Api and Recoil

--- assignment/week-7/week-7-1/src/PropDrilling.jsx
+++ assignment/week-7/week-7-1/src/PropDrilling.jsx
@@ -1,31 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react';
+import { RecoilRoot, useRecoilState, useRecoilValue } from 'recoil';
+import { countAtom } from './store/atoms/count';
 
 const PropDrilling = () => {
-    const [count, setCount] = useState(0)
+  //   const [count, setCount] = useState(0);
 
-    return (
-        <div>
-            <Count count={count} setCount={setCount} />
-        </div>
-    )
-}
+  return (
+    <div>
+      {/* <CountContext.Provider value={{ count, setCount }}> */}
+      <RecoilRoot>
+        <Count />
+      </RecoilRoot>
+      {/* </CountContext.Provider> */}
+    </div>
+  );
+};
 
-const Count = ({ count,setCount }) => {
-    return (
-        <div>{count}
-                    <Buttons count={count} setCount={setCount} />
-                    </div>
-        
-    )
-}
+const Count = () => {
+  return (
+    <div>
+      <CountRenderer />
+      <Buttons />
+    </div>
+  );
+};
 
-const Buttons = ({ count, setCount }) => {
-    return (
-        <div>
-            <button onClick={() => setCount(count - 1)}>decrease</button>
-            <button onClick={() => setCount(count + 1)}>increase</button>
-        </div>
-    )
-}
+const CountRenderer = () => {
+  // const {count} = useContext(CountContext);
+  const count = useRecoilValue(countAtom);
+  return <div>{count}</div>;
+};
 
-export default PropDrilling
+const Buttons = () => {
+  //   const {count,setCount} = useContext(CountContext)
+  const [count, setCount] = useRecoilState(countAtom);
+  return (
+    <div>
+      <button onClick={() => setCount(count - 1)}>decrease</button>
+      <button onClick={() => setCount(count + 1)}>increase</button>
+    </div>
+  );
+};
+
+export default PropDrilling;
