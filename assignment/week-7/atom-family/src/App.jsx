62909671fa22088934e14cc25b1248b73a529c79
Sadik Shaikh <abdullahsadik00@gmail.com>
2024-08-20 19:21:32 +0530

    React Basic : Recoil atom family

--- assignment/week-7/atom-family/src/App.jsx
+++ assignment/week-7/atom-family/src/App.jsx
@@ -1,22 +1,39 @@
 
 import './App.css'
-import { RecoilRoot, useRecoilState } from 'recoil';
+import { RecoilRoot, useRecoilState,useRecoilValue,useSetRecoilState } from 'recoil';
 import { todosAtomFamily } from './atoms';
+import { useEffect } from 'react';
 
 function App() {
   return <RecoilRoot>
+    <UpdaterComponent/>
     <Todo id={1}/>
+    <Todo id={2} />
+    <Todo id={2} />
     <Todo id={2} />
   </RecoilRoot>
 }
 
-function Todo({id}) {
-   const [todo, setTodo] = useRecoilState(todosAtomFamily(id));
+function UpdaterComponent(){
+const updateTodo = useSetRecoilState(todosAtomFamily(2))
+useEffect(()=>{
+  setTimeout(()=>{
+    updateTodo({
+  id: 2,
+  title: "New Todo",
+  description: "New Description"
+})
+  },2000)
+},[])
+return <div></div>
+}
 
+function Todo({id}) {
+   const currentTodo = useRecoilValue(todosAtomFamily(id));
   return (
     <>
-      {todo.title}
-      {todo.description}
+      {currentTodo.title}
+      {currentTodo.description}
       <br />
     </>
   )
