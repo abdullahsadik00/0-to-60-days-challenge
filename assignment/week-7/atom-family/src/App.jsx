e81afc25248be5916841793000913310d40f741f
Sadik Shaikh <abdullahsadik00@gmail.com>
2024-08-21 12:32:27 +0530

    React Basic : Recoil atom family selector

--- assignment/week-7/atom-family/src/App.jsx
+++ assignment/week-7/atom-family/src/App.jsx
@@ -1,42 +1,47 @@
-
-import './App.css'
-import { RecoilRoot, useRecoilState,useRecoilValue,useSetRecoilState } from 'recoil';
-import { todosAtomFamily } from './atoms';
+import './App.css';
+import {
+  RecoilRoot,
+  useRecoilValue,
+  useSetRecoilState,
+} from 'recoil';
+import { todosAtomFamily, todosAtomFamilyAsync } from './atoms';
 import { useEffect } from 'react';
 
 function App() {
-  return <RecoilRoot>
-    <UpdaterComponent/>
-    <Todo id={1}/>
-    <Todo id={2} />
-    <Todo id={2} />
-    <Todo id={2} />
-  </RecoilRoot>
+  return (
+    <RecoilRoot>
+      <UpdaterComponent />
+      <Todo id={1} />
+      <Todo id={2} />
+      <Todo id={3} />
+      <Todo id={4} />
+    </RecoilRoot>
+  );
 }
 
-function UpdaterComponent(){
-const updateTodo = useSetRecoilState(todosAtomFamily(2))
-useEffect(()=>{
-  setTimeout(()=>{
-    updateTodo({
-  id: 2,
-  title: "New Todo",
-  description: "New Description"
-})
-  },2000)
-},[])
-return <div></div>
+function UpdaterComponent() {
+  const updateTodo = useSetRecoilState(todosAtomFamily(2));
+  useEffect(() => {
+    setTimeout(() => {
+      updateTodo({
+        id: 2,
+        title: 'New Todo',
+        description: 'New Description',
+      });
+    }, 2000);
+  }, []);
+  return <div></div>;
 }
 
-function Todo({id}) {
-   const currentTodo = useRecoilValue(todosAtomFamily(id));
+function Todo({ id }) {
+  const currentTodo = useRecoilValue(todosAtomFamilyAsync(id));
   return (
     <>
       {currentTodo.title}
       {currentTodo.description}
       <br />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- assignment/week-7/atom-family/src/atoms.js
+++ assignment/week-7/atom-family/src/atoms.js
@@ -1,9 +1,22 @@
-import { atomFamily } from "recoil";
-import { TODOS } from "./todos";
+import { atomFamily, selectorFamily } from "recoil";
+import axios from "axios";
 
+import { TODOS } from "./todos";
+  
 export const todosAtomFamily = atomFamily({
   key: 'todosAtomFamily',
   default: id => {
     return TODOS.find(x => x.id === id)
   },
 });
+
+export const todosAtomFamilyAsync = atomFamily({
+  key:"todosAtomFamilyAsync",
+  default: selectorFamily({
+    key:"todosAtomSelectorFamilyAsync",
+    get:(id)=>async({get})=>{
+      const res = await axios.get(`https://sum-server.100xdevs.com/todo?id=${id}`)
+      return res.data.todo
+    }
+  })
+})
