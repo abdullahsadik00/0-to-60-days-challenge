8dbf5d62f4c7f738150355d78689d53226c44172
Sadik Shaikh <abdullahsadik00@gmail.com>
2024-09-08 23:53:17 +0530

    Typescript

---
+++ assignment/week-9/typescript/test.ts
@@ -0,0 +1,11 @@
+const x: number = 1;
+console.log(x)
+
+function greet(firstname: string){
+    console.log("Hello "+firstname)
+}
+greet("sasasd")
+function sum(a:number,b:number){
+    return a+ b;
+}
+const value = sum(2,6)
